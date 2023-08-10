97c12961f845124cf0b06b30e26a01ad087d7803
Connor Slade <connor@connorcode.com>
2023-08-10 12:22:21 -0400

    Split download into Jobs

--- boatinfo_downloader/src/main.rs
+++ boatinfo_downloader/src/main.rs
@@ -1,12 +1,14 @@
-use anyhow::{bail, Result};
+use anyhow::{bail, Error, Result};
 
 mod boat_info;
 use boat_info::BoatInfo;
 use csv::Writer;
 use indicatif::ParallelProgressIterator;
 use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
+use retry::{delay::Exponential, retry};
 use serde::Deserialize;
 
+const CHUNK_SIZE: usize = 100;
 const SEARCH_PATH: &str = "https://www.boatinfoworld.com/getsearchresults.asp";
 const STATE_CODES: &[&str] = &[
     "AL", "KY", "OH", "AK", "LA", "OK", "AZ", "ME", "OR", "AR", "MD", "PA", "AS", "MA", "PR", "CA",
@@ -16,16 +18,55 @@ const STATE_CODES: &[&str] = &[
 ];
 
 fn main() -> Result<()> {
-    let mut writer = Writer::from_path("out.csv")?;
-    let data = STATE_CODES
+    let counts = STATE_CODES
         .par_iter()
         .progress()
-        .cloned()
-        .map(download_state)
+        .map(|x| {
+            retry(Exponential::from_millis(10).take(3), || {
+                Ok::<_, Error>((x, raw_download(x, 0, 0)?.records_total))
+            })
+        })
+        .filter_map(Result::ok)
+        .collect::<Vec<_>>();
+
+    println!(
+        "Counted {} total boats.",
+        counts.iter().fold(0, |acc, x| acc + x.1)
+    );
+
+    let jobs = counts
+        .into_iter()
+        .flat_map(|(&state, total)| {
+            (0..=total)
+                .collect::<Vec<_>>()
+                .chunks(CHUNK_SIZE)
+                .map(|x| x.len())
+                .enumerate()
+                .map(|(i, x)| Job {
+                    state: state.to_owned(),
+                    start: (i * CHUNK_SIZE) as u64,
+                    count: x as u64,
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    println!("Split into {} jobs.", jobs.len());
+
+    let data = jobs
+        .par_iter()
+        .progress()
+        .map(|x| {
+            retry(Exponential::from_millis(10).take(3), || {
+                Ok::<_, Error>(download_state(&x.state, x.start, x.count))
+            })
+        })
+        .filter_map(Result::ok)
         .filter_map(Result::ok)
         .flatten()
         .collect::<Vec<_>>();
 
+    let mut writer = Writer::from_path("out.csv")?;
     for i in data {
         if let Err(e) = writer.serialize(i) {
             eprintln!("Error serializing info: {e:?}");
@@ -36,15 +77,8 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn download_state(state: &str) -> Result<Vec<BoatInfo>> {
-    let init = match raw_download(state, 0) {
-        Ok(e) => e,
-        Err(e) => {
-            eprintln!("Error getting {state} size: {e:?}");
-            bail!(e);
-        }
-    };
-    let all = match raw_download(state, init.records_total) {
+fn download_state(state: &str, start: u64, count: u64) -> Result<Vec<BoatInfo>> {
+    let all = match raw_download(state, start, count) {
         Ok(e) => e,
         Err(e) => {
             eprintln!("Error getting {state} records: {e:?}");
@@ -68,9 +102,10 @@ struct RawDownload {
     data: Vec<Vec<String>>,
 }
 
-fn raw_download(state: &str, count: u64) -> Result<RawDownload> {
+fn raw_download(state: &str, start: u64, count: u64) -> Result<RawDownload> {
     let body = include_str!("../search.txt")
         .replacen("{{STATE}}", state, 1)
+        .replacen("{{START}}", &start.to_string(), 1)
         .replacen("{{COUNT}}", &count.to_string(), 1);
 
     let res = minreq::post(SEARCH_PATH)
@@ -79,4 +114,11 @@ fn raw_download(state: &str, count: u64) -> Result<RawDownload> {
         .send()?;
 
     Ok(res.json()?)
+}
+
+#[derive(Debug)]
+struct Job {
+    state: String,
+    start: u64,
+    count: u64,
 }
