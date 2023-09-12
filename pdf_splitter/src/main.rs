1ee372759d29b84e9c59614ba02223dddd81a7f8
Connor Slade <connor@connorcode.com>
2023-09-12 18:47:43 -0400

    Try to create new pdf

--- pdf_splitter/src/main.rs
+++ pdf_splitter/src/main.rs
@@ -1,15 +1,15 @@
-use std::{borrow::Cow, cell::RefCell, collections::BTreeMap, fs, ops::Range};
+use std::{borrow::Cow, cell::RefCell, collections::BTreeMap, fs, ops::Range, path::PathBuf};
 
 use anyhow::Context;
 use indicatif::ProgressIterator;
-use lopdf::{Destination, Document, Outline};
+use lopdf::{dictionary, Destination, Document, Object, Outline, Stream};
 use splitter::Splitter;
 
 mod splitter;
 
 const INP_FILE: &str =
     r"V:\Downloads\Ron Larson - Precalculus with Limits-Cengage Learning (2013).pdf";
-const OUT_DIR: &str = "output";
+const OUT_DIR: &str = "./output/";
 
 struct Section {
     pub level: usize,
@@ -20,7 +20,7 @@ struct Section {
 
 struct SplitterJob {
     doc: Document,
-    filename: String,
+    filename: PathBuf,
     pages: Range<usize>,
 }
 
@@ -64,7 +64,7 @@ fn main() -> anyhow::Result<()> {
         };
 
         if splitter.should_split(&section) {
-            let filename = format!("{}/{}.pdf", OUT_DIR, section.name);
+            let filename = PathBuf::from(OUT_DIR).join(format!("{}.pdf", splitter.name(&section)));
             let pages = section.start..section.end;
 
             jobs.push(SplitterJob {
@@ -84,15 +84,66 @@ fn main() -> anyhow::Result<()> {
     //  .progress_count(job_count)
     jobs.into_iter().progress_count(job_count).for_each(|job| {
         let mut doc = Document::new();
+        let pages_id = doc.new_object_id();
 
+        // == todo
+        let font_id = doc.add_object(dictionary! {
+            // type of dictionary
+            "Type" => "Font",
+            // type of font, type1 is simple postscript font
+            "Subtype" => "Type1",
+            // basefont is postscript name of font for type1 font.
+            // See PDF reference document for more details
+            "BaseFont" => "Courier",
+        });
+
+        let resources_id = doc.add_object(dictionary! {
+            // fonts are actually triplely nested dictionaries. Fun!
+            "Font" => dictionary! {
+                // F1 is the font name used when writing text.
+                // It must be unique in the document. It does not
+                // have to be F1
+                "F1" => font_id,
+            },
+        });
+        // ==
+
+        let mut pages = Vec::new();
         for i in job.pages {
             let page_id = job.doc.page_iter().nth(i).unwrap();
             let page = job.doc.get_page_content(page_id).unwrap();
-            doc.add_page_contents(page_id, page).unwrap();
+
+            let content_id = doc.add_object(Stream::new(dictionary! {}, page));
+            let page_id = doc.add_object(dictionary! {
+                "Type" => "Page",
+                "Parent" => pages_id,
+                "Contents" => content_id,
+            });
+            pages.push(page_id.into());
         }
 
+        let pages = dictionary! {
+            "Type" => "Pages",
+            "Kids" => dbg!(pages),
+            "Count" => 1,
+            "Resources" => resources_id,
+            // a rectangle that defines the boundaries of the physical or digital media. This is the
+            // "Page Size"
+            "MediaBox" => vec![0.into(), 0.into(), 595.into(), 842.into()],
+        };
+
+        doc.objects.insert(pages_id, Object::Dictionary(pages));
+
+        let catalog_id = doc.add_object(dictionary! {
+            "Type" => "Catalog",
+            "Pages" => pages_id,
+        });
+
+        doc.trailer.set("Root", catalog_id);
+        // doc.compress();
+
         if let Err(e) = doc.save(&job.filename) {
-            eprintln!("Error saving {}: {}", job.filename, e);
+            eprintln!("Error saving {:?}: {}", job.filename, e);
         }
     });
 
