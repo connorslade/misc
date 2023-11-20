5887820a6ea80643ac9d988d0e9cf6d956a3da98
Connor Slade <connor@connorcode.com>
2023-11-19 20:49:38 -0500

    Add READMES

---
+++ test_score_scraper/README.md
@@ -0,0 +1,5 @@
+# Test Score Scraper
+
+Tools for scraping and analyzing standardized test score and GPA data from [Naviance](https://naviance.com).
+
+![](https://github.com/Basicprogrammer10/misc/assets/50306817/2bc1e078-76c6-4cae-bc1a-7ee83f39c31e)
--- test_score_scraper/statistics.jl
+++ test_score_scraper/statistics.jl
@@ -3,11 +3,7 @@ using StatsPlots
 
 local data = JSON.parsefile("test_score_scraper/data.json")
 
-sat = []
-act = []
-gpa = []
-
-function process_test(data)
+function process_test(data, stats)
     if isnothing(data)
         return nothing
     end
@@ -26,7 +22,7 @@ function process_test(data)
         end
 
         for student in students
-            for (list, index) in [(gpa, "gpa"), (sat, "highestComboSat"), (act, "actCompositeStudent")]
+            for (list, index) in stats
                 local value = student[index]
                 if isnothing(value) || value == 0
                     continue
@@ -38,33 +34,47 @@ function process_test(data)
 end
 
 
-for (collage_id, college) in data
-    local scattergrams = college[2]["scattergrams"]
-    if isnothing(scattergrams)
-        continue
+function main()
+    local sat = []
+    local act = []
+    local gpa = []
+
+    local stats = [
+        (gpa, "gpa"),
+        (sat, "highestComboSat"),
+        (act, "actCompositeStudent")
+    ]
+
+    for college in values(data)
+        local scattergrams = college[2]["scattergrams"]
+        if isnothing(scattergrams)
+            continue
+        end
+
+        local weighted_gpa = scattergrams["weightedGpa"]
+        if isnothing(weighted_gpa)
+            continue
+        end
+
+        println("Processing $(college[1]["name"])")
+        process_test(weighted_gpa["sat"], stats)
+        process_test(weighted_gpa["act"], stats)
     end
 
-    local weighted_gpa = scattergrams["weightedGpa"]
-    if isnothing(weighted_gpa)
-        continue
-    end
 
-    println("Processing $(college[1]["name"])")
-    process_test(weighted_gpa["sat"])
-    process_test(weighted_gpa["act"])
+    println()
+    println("[*] Found $(length(sat)) SAT scores")
+    println("[*] Found $(length(act)) ACT scores")
+    println("[*] Found $(length(gpa)) GPAs")
+
+
+    plot(
+        boxplot(["sat"], sat),
+        boxplot(["act"], act),
+        boxplot(["gpa"], gpa),
+        size=(1000, 750),
+        layout=(1, 3)
+    )
 end
 
-
-println()
-println("[*] Found $(length(sat)) SAT scores")
-println("[*] Found $(length(act)) ACT scores")
-println("[*] Found $(length(gpa)) GPAs")
-
-
-plot(
-    boxplot(["sat"], sat),
-    boxplot(["act"], act),
-    boxplot(["gpa"], gpa),
-    size = (750, 500),
-    layout = (1, 3)
-)
+main()
