9b6336998b552335ef228903994ed05fd7fd5e59
Connor Slade <connor@connorcode.com>
2023-12-10 15:14:17 -0500

    projectile_sim: App Struct

--- projectile_sim/src/main.rs
+++ projectile_sim/src/main.rs
@@ -1,22 +1,33 @@
 use macroquad::{
     color,
+    input::{is_mouse_button_down, mouse_delta_position},
     math::{vec2, Vec2},
+    miniquad::MouseButton,
     shapes::{draw_circle, draw_line},
     ui::{hash, root_ui, widgets},
     window::{self, clear_background, next_frame, screen_height, screen_width},
 };
 use ordered_float::OrderedFloat;
 
-const MIN: f32 = 0.001;
+struct State {
+    x_offset: f32,
+    y_offset: f32,
+    initial_velocity: f32,
+    initial_angle: f32,
+    acceleration: f32,
+    steps: f32,
+}
 
 #[macroquad::main("ProjectileSim")]
 async fn main() {
-    let mut x_offset = 0.0;
-    let mut y_offset = 0.0;
-    let mut initial_velocity = 5.0;
-    let mut initial_angle = 45.0;
-    let mut acceleration = 9.8;
-    let mut steps = 1.0;
+    let mut state = State {
+        x_offset: 0.0,
+        y_offset: 0.0,
+        initial_velocity: 5.0,
+        initial_angle: 45.0,
+        acceleration: 9.8,
+        steps: 20.0,
+    };
 
     loop {
         let (width, height) = (window::screen_width(), window::screen_height());
@@ -26,24 +37,35 @@ async fn main() {
             .label("Config")
             .titlebar(true)
             .ui(&mut *root_ui(), |ui| {
-                ui.slider(hash!(), "Vi", MIN..150f32, &mut initial_velocity);
-                ui.slider(hash!(), "theta", -90f32..90f32, &mut initial_angle);
-                ui.slider(hash!(), "accel", 0f32..20f32, &mut acceleration);
-                ui.slider(hash!(), "steps", 1f32..10f32, &mut steps);
+                ui.slider(hash!(), "Vi", 0.001..150f32, &mut state.initial_velocity);
+                ui.slider(hash!(), "theta", -90f32..90f32, &mut state.initial_angle);
+                ui.slider(hash!(), "accel", 0f32..20f32, &mut state.acceleration);
+                ui.slider(hash!(), "steps", 1f32..10f32, &mut state.steps);
                 ui.separator();
-                ui.slider(hash!(), "x offset", 0f32..width, &mut x_offset);
+                ui.slider(hash!(), "x offset", 0f32..width, &mut state.x_offset);
                 ui.slider(
                     hash!(),
                     "y offset",
                     height / -2.0..height / 2.0,
-                    &mut y_offset,
+                    &mut state.y_offset,
                 );
             });
 
-        steps = steps.round();
-        let mut points = simulate(steps as u32, initial_velocity, -initial_angle, acceleration);
+        let delta = mouse_delta_position();
+        if is_mouse_button_down(MouseButton::Middle) {
+            state.x_offset -= delta.x * width * 0.1;
+            state.y_offset -= delta.y * height * 0.1;
+        }
+
+        let steps = state.steps.round();
+        let mut points = simulate(
+            steps as u32,
+            state.initial_velocity,
+            -state.initial_angle,
+            state.acceleration,
+        );
         for point in points.iter_mut() {
-            *point = *point + vec2(x_offset, y_offset);
+            *point = *point + vec2(state.x_offset, state.y_offset);
         }
 
         let mut i = 0;
@@ -66,13 +88,18 @@ async fn main() {
 
         draw_line(
             0.0,
-            y_offset + height / 2.0,
+            state.y_offset + height / 2.0,
             screen_width(),
-            y_offset + height / 2.0,
+            state.y_offset + height / 2.0,
             1.0,
             color::GRAY,
         );
-        draw_circle(x_offset, y_offset + height / 2.0, 5.0, color::BLUE);
+        draw_circle(
+            state.x_offset,
+            state.y_offset + height / 2.0,
+            5.0,
+            color::BLUE,
+        );
 
         let highest = points
             .iter()
