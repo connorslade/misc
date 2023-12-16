f98575902591039fa24a0a3b26b996df21b8e287
Connor Slade <connor@connorcode.com>
2023-12-16 13:25:15 -0500

    grade_history:  Add plotting

--- grade_history/src/main.rs
+++ grade_history/src/main.rs
@@ -1,10 +1,18 @@
 use std::collections::HashMap;
 
 use chrono::NaiveDate;
+use plotters::{
+    backend::SVGBackend,
+    drawing::IntoDrawingArea,
+    element::Rectangle,
+    series::LineSeries,
+    style::{Palette, Palette99},
+};
 use scraper::{element_ref::Select, Html};
 use ureq::AgentBuilder;
 
 const BASE_PAGE: &str = "https://parents.c2.genesisedu.net/bernardsboe";
+const OUTPUT: &str = "output.svg";
 
 const USERNAME: &str = "";
 const PASSWORD: &str = "";
@@ -66,7 +74,75 @@ fn main() {
         });
     }
 
-    dbg!(classes);
+    let (first_date, last_date) = get_date_range(&classes);
+    let min_grade = classes
+        .values()
+        .flatten()
+        .map(|g| g.grade.0 as f64 / g.grade.1 as f64 * 100.0)
+        .min_by(|a, b| a.partial_cmp(b).unwrap())
+        .unwrap();
+    for grades in classes.values_mut() {
+        grades.sort_by_key(|g| g.date);
+    }
+
+    println!(
+        "Found {} classes and {} total assignments.",
+        classes.len(),
+        classes.values().map(|v| v.len()).sum::<usize>()
+    );
+
+    let root = SVGBackend::new(OUTPUT, (1024, 768)).into_drawing_area();
+    root.fill(&plotters::style::RGBColor(255, 255, 255))
+        .unwrap();
+
+    let mut chart = plotters::chart::ChartBuilder::on(&root)
+        .margin(5)
+        .caption("Grade History", ("sans-serif", 50))
+        .x_label_area_size(40)
+        .y_label_area_size(40)
+        .build_cartesian_2d(first_date..last_date, min_grade..100.0)
+        .unwrap();
+
+    chart
+        .configure_mesh()
+        .x_labels(30)
+        .y_labels(10)
+        .y_desc("Grade (%)")
+        .x_desc("Date")
+        .draw()
+        .unwrap();
+
+    // Draw dashed line at 90%
+    chart
+        .draw_series(LineSeries::new(
+            vec![(first_date, 90.0), (last_date, 90.0)],
+            plotters::style::RGBColor(0, 0, 0),
+        ))
+        .unwrap();
+
+    let mut classes = classes.into_iter().collect::<Vec<_>>();
+    classes.sort_by_key(|(class, _)| *class);
+
+    for (idx, (class, grades)) in classes.into_iter().enumerate() {
+        let mut points = Vec::new();
+        let mut acc = (0, 0);
+        for grade in grades {
+            acc.0 += grade.grade.0;
+            acc.1 += grade.grade.1;
+            points.push((grade.date, acc.0 as f64 / acc.1 as f64 * 100.0));
+        }
+
+        let color = Palette99::pick(idx);
+        chart
+            .draw_series(LineSeries::new(points.into_iter(), &color))
+            .unwrap()
+            .label(class)
+            .legend(move |(x, y)| Rectangle::new([(x - 5, y - 5), (x + 5, y + 5)], &color));
+    }
+
+    chart.configure_series_labels().draw().unwrap();
+
+    root.present().unwrap();
 }
 
 fn get_text<'a, 'b>(mut item: Select<'a, 'b>) -> &'a str {
@@ -87,4 +163,17 @@ fn parse_grade(grade: &str) -> Option<(u32, u32)> {
 fn parse_date(date: &str) -> Option<NaiveDate> {
     let (month, day) = date.split_once("/")?;
     NaiveDate::from_ymd_opt(2023, month.parse().ok()?, day.parse().ok()?)
+}
+
+fn get_date_range(classes: &HashMap<&str, Vec<Grade>>) -> (NaiveDate, NaiveDate) {
+    let (mut min, mut max) = (NaiveDate::MAX, NaiveDate::MIN);
+
+    for grades in classes.values() {
+        for grade in grades {
+            min = min.min(grade.date);
+            max = max.max(grade.date);
+        }
+    }
+
+    (min, max)
 }
